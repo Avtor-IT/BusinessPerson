f4a6e31b691932aefe809f873a079f0d0f9079ff
AnastasiaManaeva <tvoyasovushka@yandex.ru>
2024-07-31 14:50:52 +0500

    slider is still not workingD

--- src/features/SliderRecomeds/ui/SliderRecomeds.jsx
+++ src/features/SliderRecomeds/ui/SliderRecomeds.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
+import Slider from "react-slick";
 import { getArrayRecomends } from 'entities/ArrayRecomends';
-import { Box, Button, MobileStepper, Paper, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import cls from './sliderRecomends.module.scss';
 import { Card } from 'shared/ui/Card';
-import SwipeableViews from 'react-swipeable-views';
+import { height } from '@mui/system';
 
 export const SliderRecomends = ({ ...props }) => {
 
     const testArrayRecomends = getArrayRecomends();
 
-    const [activeStep, setActiveStep] = React.useState(0);
-
-    const handleStepChange = (step) => {
-        setActiveStep(step);
+    const settings = {
+        dots: false,
+        accessibility: false,
+        arrows: false,
+        // infinite: true,
+        // speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dir: 'rtl',
     };
+
     return (
-        <Box>
-            <SwipeableViews
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-                slideCount={3}
-                style={{ overflowX: 'visible' }}
-                className={`${cls.sliderRecomends}`}
-            >
+        <Box className={`${cls.sliderRecomends}`}>
+            <Slider {...settings} style={{ width: '923px', height: '257px', display: 'flex' }}>
                 {testArrayRecomends.map((item, index) => (
-                    Math.abs(activeStep - index) <= 2 ? (
-                        <Card className={cls.recomendItem} key={index}>
-                            <Typography variant='M20'>{item.title}</Typography>
-                            <Typography variant='R16'>{item.description}</Typography>
-                        </Card>
-                    ) : null
+                    <Card className={cls.recomendItem} key={index}>
+                        <Typography variant='M20'>{item.title}</Typography>
+                        <Typography variant='R16'>{item.description}</Typography>
+                    </Card>
                 ))}
-            </SwipeableViews>
+            </Slider>
         </Box>
     );
 };
