c646b609709f617f4dd876aefb772c1489e71459
AnastasiaManaeva <tvoyasovushka@yandex.ru>
2024-08-01 12:21:56 +0500

    слайдер готов

--- src/features/SliderRecomeds/index.js
+++
@@ -1,1 +0,0 @@
-export { SliderRecomends } from './ui/SliderRecomeds';
--- src/widgets/MyRecomends/ui/MyRecomends.jsx
+++ src/widgets/MyRecomends/ui/MyRecomends.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { getArrayRecomends } from 'entities/ArrayRecomends';
 import { Card } from 'shared/ui/Card';
-import { Typography } from '@mui/material';
-import { SliderRecomends } from 'features/SliderRecomeds';
+import Slider from 'react-slick';
 import cls from './MyRecomends.module.scss';
+import './myRecomends.scss';
 
 export const MyRecomends = ({ ...props }) => {
+    const testArrayRecomends = getArrayRecomends();
+
+    const settings = {
+        dots: false,
+        accessibility: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        infinite: false,
+    };
+
     return (
         <Card
             className={`${cls.MyRecomends}`}
             {...props}
         >
             <Typography variant="M24">Рекомендации</Typography>
-            <SliderRecomends />
+            <Box className={`${cls.sliderRecomends}`}>
+                <Slider
+                    {...settings}
+                    style={{ width: '923px' }}
+                >
+                    {testArrayRecomends.map((item, index) => (
+                        <Card
+                            className={cls.recomendItem}
+                            key={index}
+                        >
+                            <Typography variant="M20">{item.title}</Typography>
+                            <Typography variant="R16">
+                                {item.description}
+                            </Typography>
+                        </Card>
+                    ))}
+                </Slider>
+            </Box>
         </Card>
     );
 };
