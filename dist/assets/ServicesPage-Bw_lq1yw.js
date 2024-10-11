f645495a6199df1391ae45eb069e7364a3053257
Holakarma <holakarmos@gmail.com>
2024-10-11 11:28:59 +0500

    add(dist) prepared & built

--- dist/assets/ServicesPage-Bw_lq1yw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CKaufg0w.js";import{S as e}from"./ServiceSection-BQg-gRf-.js";import"./Typography-DO0TQFhO.js";const i=()=>r.jsx(e,{});export{i as default};
--- dist/assets/TutorialPage-CImTjRh5.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CKaufg0w.js";const a=()=>r.jsx("div",{children:"Tutorial page"});export{a as default};
