64d893dc67b48378a2e752037e82d077f4cf6849
Holakarma <holakarmos@gmail.com>
2025-01-24 15:59:09 +0500

    [docs]

---
+++ docs/assets/LetterPage-CFZUClr2.js
@@ -0,0 +1,1 @@
+import{j as r,T as t}from"./index-u4m07UPw.js";const e=()=>r.jsx(t,{variant:"M40",children:"Конструктор письма"});export{e as default};
--- docs/assets/TutorialPage-Dn59yfGO.js
+++
@@ -1,1 +0,0 @@
-import{j as a,z as r}from"./index-Df1ayVmF.js";const o=()=>a.jsx(r,{variant:"M40",children:"Tutorial page"});export{o as default};
