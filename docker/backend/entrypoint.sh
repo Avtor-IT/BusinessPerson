d4ff1d908a02189c4a41aeef1296f7085dd1ad30
Holakarma <holakarmos@gmail.com>
2025-03-31 11:24:17 +0500

    [fix]: added chat route to nginx.conf

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "migration started..."
 python3 manage.py makemigrations  || exit 1
 python3 manage.py migrate || exit 1
 uvicorn drf_auth.asgi:application --host 0.0.0.0 --port 8000 
