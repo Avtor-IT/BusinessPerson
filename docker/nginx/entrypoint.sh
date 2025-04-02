14d4a98cb29a0ea09b986517faf4aac603d5ab19
Holakarma <holakarmos@gmail.com>
2025-04-02 16:38:32 +0500

    [fix]

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-until wget --no-verbose --tries=1 --spider http://127.0.0.1:8000; do
+until curl --fail http://127.0.0.1:3000; do
+    echo "Waiting for frontend..."
+    sleep 10
+done
+
+until curl --fail http://127.0.0.1:8000; do
     echo "Waiting for backend..."
     sleep 10
 done
 
-until wget --no-verbose --tries=1 --spider http://127.0.0.1:3000; do
-    echo "Waiting for backend..."
-    sleep 10
-done
 
 nginx-debug -g "daemon off";
 
