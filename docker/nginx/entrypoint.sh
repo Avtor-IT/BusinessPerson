fca8c0f5e9a327d84e7f57a134c57c94f2b99936
Holakarma <holakarmos@gmail.com>
2025-04-03 14:19:40 +0500

    [ci]

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-until curl http://frontend:3000; do
+until curl -s http://frontend:3000; do
     echo "Waiting for frontend..."
     sleep 10
 done
 
-until curl http://backend:8000; do
+until curl -s http://backend:8000; do
     echo "Waiting for backend..."
     sleep 10
 done
