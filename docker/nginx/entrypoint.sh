7bedfbb95465844f0a86bdc45d6e22ead8088b02
Holakarma <holakarmos@gmail.com>
2025-04-03 12:23:11 +0500

    [ci]

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-until curl -s --fail http://frontend:3000; do
-    echo "Waiting for frontend..."
-    sleep 10
-done
-
-until curl -s --fail http://backend:8000; do
-    echo "Waiting for backend..."
-    sleep 10
-done
-
-
 nginx-debug -g "daemon off";
 
 exec "$@"
