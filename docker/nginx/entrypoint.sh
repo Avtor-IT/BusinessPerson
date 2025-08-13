d534ddac4b45dccd5b662698d08b5ee53a233b5a
Holakarma <holakarmos@gmail.com>
2025-08-13 16:04:09 +0500

    ssl

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# until curl -s http://frontend:3000; do
-#     echo "Waiting for frontend..."
-#     sleep 10
-# done
-
 until curl -s http://backend:8000; do
     echo "Waiting for backend..."
     sleep 10
